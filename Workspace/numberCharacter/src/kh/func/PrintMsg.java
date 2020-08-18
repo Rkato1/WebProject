7643b84b1b3b7e2f2898ca4aeeb66144e7810e94
Rkato1 <36435326+Rkato1@users.noreply.github.com>
2020-08-18 14:09:48 +0900

    add

--- Workspace/numberCharacter/src/kh/func/PrintMsg.java
+++ Workspace/numberCharacter/src/kh/func/PrintMsg.java
@@ -1,5 +1,13 @@
 package kh.func;
 
 public class PrintMsg {
+	public void print() {
+		System.out.println(1+1);
+		System.out.println(1.1+1.1);
+		System.out.println('1'+'1'); //49+49, ascii code
+		//문자의 값의 합
+		System.out.println("1"+"1"); //1&1
+		//문자 열의 합산
+	}
 
 }
--- Workspace/numberCharacter/src/kh/java/Start.java
+++ Workspace/numberCharacter/src/kh/java/Start.java
@@ -1,10 +1,13 @@
 package kh.java;
 
+import kh.func.PrintMsg;
+
 public class Start {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		PrintMsg pm = new PrintMsg();
+		pm.print();
 	}
 
 }
