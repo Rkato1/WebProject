a030bfd33fd6de27073edcdd2e7db2d082af0fe6
Rkato1 <36435326+Rkato1@users.noreply.github.com>
2020-08-18 15:35:31 +0900

    add

--- Workspace/numberCharacter/src/kh/func/PrintMsg.java
+++ Workspace/numberCharacter/src/kh/func/PrintMsg.java
@@ -3,8 +3,9 @@ package kh.func;
 public class PrintMsg {
 	public void print() {
 		System.out.println(1+1);
-		System.out.println(1.1+1.1);
-		System.out.println('1'+'1'); //49+49, ascii code
+		System.out.println(1+1.1);
+		//강제 형변환되어 소수점으로 나옴
+		System.out.println('1'+1); //49+1, ascii code
 		//문자의 값의 합
 		System.out.println('1'-'2'); //49-50, ascii code
 		System.out.println("1"+"1"); //1&1
