88d6f5e101d1e15c874c72df6b542b6ab8d9dbdc
Rkato1 <36435326+Rkato1@users.noreply.github.com>
2020-08-18 17:00:58 +0900

    add

--- Workspace/variable/src/kh/func/Variable.java
+++ Workspace/variable/src/kh/func/Variable.java
@@ -27,5 +27,24 @@ public class Variable {
 		//문자열 변수
 		System.out.println(str.charAt(0));
 		//이 변수의 첫번째 자리만 출력하기
+		
+		int num1 = 2147483647;
+		long res = (long)num1 + 1;
+		//int 범위를 초과했기 때문에 원하는 값을 도출하기 위해 강제 형변환을 해줘야함
+		System.out.println(res);
+		
+		byte b1 = 10;
+		byte b2 = 20;
+		//byte b3 = b1 + b2;
+		//java에서 계산시 기본 int로 치환해서 오류
+		int b3 = b1 + b2;
+		byte b4 = (byte)(b1 + b2);
+		//해결 방법 , 위는 연산 이후 형변환, 아래는 형변환 이후 연산(처리가 조금 다름, 범위 초과우려)
+		
+		System.out.println("11");
+		System.out.println("22");
+		System.out.print("11");
+		System.out.print("22");
+		//println은 출력 후 줄 바꿈, print는 출력만
 	}
 }
