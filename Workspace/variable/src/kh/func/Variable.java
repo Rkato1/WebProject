ec1c660af79babd5960ee067b3ad4b4332272259
Rkato1 <36435326+Rkato1@users.noreply.github.com>
2020-08-18 15:51:11 +0900

    add

--- Workspace/variable/src/kh/func/Variable.java
+++ Workspace/variable/src/kh/func/Variable.java
@@ -1,5 +1,14 @@
 package kh.func;
 
 public class Variable {
-
+	public void func1() {
+		boolean bool1 = true;
+		boolean bool2 = false;
+		//논리형 변수
+		char ch1 = 'a';
+		//문자 변수
+		int num = 10;
+		//정수형 변수
+		System.out.println(ch1);
+	}
 }
--- Workspace/variable/src/kh/java/Start.java
+++ Workspace/variable/src/kh/java/Start.java
@@ -1,9 +1,12 @@
 package kh.java;
 
+import kh.func.Variable;
+
 public class Start {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Variable v = new Variable();
+		v.func1();
 	}
 }
