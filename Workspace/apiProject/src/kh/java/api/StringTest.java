af0694d9b74c13e2d5d9f92d7022350aa10c08ad
Rkato1 <36435326+Rkato1@users.noreply.github.com>
2020-09-01 18:22:46 +0900

    0901_final

--- Workspace/apiProject/src/kh/java/api/StringTest.java
+++ Workspace/apiProject/src/kh/java/api/StringTest.java
@@ -1,5 +1,97 @@
 package kh.java.api;
 
-public class StringTest {
+import java.util.StringTokenizer;
 
+public class StringTest {
+	public void stringBufferTest() {
+		StringBuffer sb = new StringBuffer();
+		//할당된 문자열 저장소의 길이
+		System.out.println(sb.capacity());
+		//실제 저장된 문자열의 길이
+		System.out.println(sb.length());
+		
+		//StringBuffer로 할당
+		//문자열의 끝에 추가되는 개념임
+		sb.append("문자열할당");
+		System.out.println(sb.length());
+		sb.append(" 공 백 ");
+		System.out.println(sb.length());
+		sb.append("추가");
+		System.out.println(sb.length());
+		System.out.println(sb);
+		
+		//sb.insert(offset, c)
+		//삽입하는 메소드(위치, 삽입할 문자열)		
+		sb.insert(5, "a");
+		System.out.println(sb);
+		
+		//sb.replace(start, end, str)
+		//대체하는 개념
+		//시작점, 끝점, 대체할 문자열
+		sb.replace(5, 6, "b");
+		System.out.println(sb);
+		
+		//뒤집기
+		sb.reverse();
+		System.out.println(sb);
+		sb.reverse();
+		System.out.println(sb);
+		
+		//삭제(시작점과 끝점)
+		//sb.delete(start, end)
+		sb.delete(5, sb.length());
+		System.out.println(sb);
+	}
+	
+	public void token() {
+		String str = "a/20/ㅎb cㅇ";
+		//구분자를 기준으로 문자열을 나눠서 받는개념(문자열,구분자)
+		//참조형이라 실제 데이터를 분할하는 개념이 아님
+		StringTokenizer st = new StringTokenizer(str, "/");
+		//st.countTokens()
+		//구분자로 나눠서 총 몇개로 나눠졌는지 개수를 반환
+		System.out.println(st.countTokens());//3
+		//토큰꺼내옴 문자열 리턴(0번 인수에 있는)
+		System.out.println(st.nextToken());
+		//현재 남아있는 토큰이 있는지 확인후 반환(t/f)
+		System.out.println(st.hasMoreTokens());
+		
+		//다 뽑힐때까지 나옴
+		/*
+		while(st.hasMoreTokens()) {
+			System.out.println(st.nextToken());
+		}
+		*/
+	}
+	
+	public void tokenExam() {
+		String str = "J a v a P r o g r a m";
+		char ch[] = new char[str.length()];
+		/*
+		//강사님방법
+		StringBuffer sb = new StringBuffer();
+		char arr[] = new char[st.countTokens()];
+		for(int j=0; j<arr.length;j++) {
+			arr[j] = st.nextToken().charAt(0);
+			if('a'<=arr[j] && arr[j]<='z') {
+				arr[i] ^= 32;
+			}
+			sb.append(arr[j]);
+		}
+		System.out.println(sb);
+		*/
+		int i=0;
+		StringTokenizer st = new StringTokenizer(str, " ");
+		while(st.hasMoreTokens()) {
+			ch[i] = st.nextToken().charAt(0);
+			i++;
+		}
+		for(int j=0;j<ch.length;j++) {
+			if(ch[j] >= 'a' && ch[j] <= 'z') { 
+				System.out.print((char)(ch[j]-32));
+			}else {
+				System.out.print(ch[j]);
+			}
+		}	
+	}	
 }
--- Workspace/apiProject/src/kh/java/run/ApiStart.java
+++ Workspace/apiProject/src/kh/java/run/ApiStart.java
@@ -1,10 +1,15 @@
 package kh.java.run;
 
+import kh.java.api.StringTest;
+
 public class ApiStart {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		StringTest st = new StringTest();
+		st.stringBufferTest();
+		st.token();
+		st.tokenExam();
 	}
 
 }
